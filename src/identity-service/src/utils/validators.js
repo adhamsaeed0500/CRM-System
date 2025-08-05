f34f46162a47eacd384a95bd4568469bdfe5b0a8
Adham Saeed <adhamsaeeed0500@gmail.com>
2025-08-05 05:54:10 +0300

    faet(identity): add createUserByAdmin Validation using Joi

--- src/identity-service/src/utils/validators.js
+++ src/identity-service/src/utils/validators.js
@@ -5,6 +5,16 @@ const validateRegistration = (data) => {
         name:Joi.string().min(2).max(60).required(),
         email:Joi.email().required(),
         password:Joi.string().min(5).required(),
+    });
+
+    return schema.validate(data);
+};
+
+const validateRegistrationByAdmin = (data) => {
+    const schema = Joi.object({
+        name:Joi.string().min(2).max(60).required(),
+        email:Joi.email().required(),
+        password:Joi.string().min(5).required(),
         role:Joi.string().required()
     });
 
