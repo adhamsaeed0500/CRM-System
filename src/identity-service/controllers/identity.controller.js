fdb0487bcb30a7618a2c7262d07d9f60a1b56595
Adham Saeed <adhamsaeeed0500@gmail.com>
2025-08-05 04:53:39 +0300

    fix(identity): return missing JWT access and refresh tokens in registration

--- src/identity-service/controllers/identity.controller.js
+++ src/identity-service/controllers/identity.controller.js
@@ -1,6 +1,7 @@
 const logger = require('../utils/logger');
 const { validateRegistration } = require('../utils/validators');
 const User = require("../models/User");
+const generateTokens = require("../utils/generateToken");
 
 const registerUser = async(req, res) => {
     logger.info("strating Registration endpoint");
@@ -26,13 +27,18 @@ const registerUser = async(req, res) => {
              });
     }   
 
+
          user = new User({ name, email, password });
          await user.save();
          logger.info("Registration - User saved successfully", user._id);
 
+         const { accessToken, refreshToken } = await generateTokens(user);
+
          res.status(201).json({
             success: true,
             userId: User._id,
+            accessToken,
+            refreshToken,
             message: "User registered successfully!",
         });
 
