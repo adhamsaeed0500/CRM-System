ce2ce60a19f7bf0baba6342ed610224358a75893
Adham Saeed <adhamsaeeed0500@gmail.com>
2025-08-05 06:00:59 +0300

    feat(identity): implement createUserByAdmin endpoint for admin user creation

--- src/identity-service/src/controllers/identity.controller.js
+++ src/identity-service/src/controllers/identity.controller.js
@@ -1,5 +1,5 @@
 const logger = require('../utils/logger');
-const { validateRegistration } = require('../utils/validators');
+const { validateRegistration, validateRegistrationByAdmin } = require('../utils/validators');
 const User = require("../models/User");
 const generateTokens = require("../utils/generateToken");
 
@@ -52,4 +52,50 @@ const registerUser = async(req, res) => {
     }
 };
 
-module.exports = { registerUser }
+const createUserByAdmin = async(req, res) => {
+    logger.info("strating RegistrationByAdmin endpoint");
+
+    try {
+        const { error } = validateRegistrationByAdmin(req.body);
+        if (error) {
+            logger.warn("RegistrationByAdmin Validation error", error.details[0].message);
+            return res.status(400).json({
+               success: false,
+               message: error.details[0].message,
+           });        
+        }
+
+        const { email, password, name, role } = req.body;
+
+         let user = await User.findOne({ $or: [{ email }, { name }] });
+        if (user) {
+            logger.warn("RegistrationByAdmin - User already exists");
+            return res.status(400).json({
+                success: false,
+                message: "User already exists",
+             });
+    }   
+
+
+         user = new User({ name, email, password, role });
+         await user.save();
+         logger.info("RegistrationByAdmin - User saved successfully", user._id);
+
+
+         res.status(201).json({
+            success: true,
+            userId: User._id,
+            message: "User registered successfully!",
+        });
+
+    } catch (err) {
+        logger.warn("RegistrationByAdmin error occured" , err);
+        res.status(500).json({
+        success: false,
+        message: "Internal server error",
+    });
+        
+    }
+};
+
+module.exports = { registerUser, createUserByAdmin }
