af48f33e4811cd31415ca130a3425ac0b863b905
Adham Saeed <adhamsaeeed0500@gmail.com>
2025-08-07 11:26:36 +0300

    chore(identity): configure server.js with initial setup

---
+++ src/identity-service/src/server.js
@@ -0,0 +1,50 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const logger = require("./utils/logger");
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const { RateLimiterRedis } = require("rate-limiter-flexible");
+const Redis = require("ioredis");
+const { rateLimit } = require("express-rate-limit");
+const { RedisStore } = require("rate-limit-redis");
+const routes = require("./routes/identity.routes");
+const errorHandler = require("./middleware/errorHandler");
+const passport = require('passport');
+require('./middlewares/passport');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//connect to mongodb
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => logger.info("Connected to mongodb"))
+  .catch((e) => logger.error("Mongo connection error", e));
+
+//middleware
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.use((req, res, next) => {
+  logger.info(`Received ${req.method} request to ${req.url}`);
+  logger.info(`Request body, ${req.body}`);
+  next();
+});
+
+app.use(passport.initialize());
+//Routes
+app.use("/", routes);
+
+//error handler
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  logger.info(`Identity service running on port ${PORT}`);
+});
+
+//unhandled promise rejection
+process.on("unhandledRejection", (reason, promise) => {
+  logger.error("Unhandled Rejection at", promise, "reason:", reason);
+});
