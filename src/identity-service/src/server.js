5fd45f0e5efe4cf2c1573c0dc13a17f9ba655e44
Adham Saeed <adhamsaeeed0500@gmail.com>
2025-08-07 11:59:39 +0300

    feat(identity): implement DDoS protection and IP-based rate limiting for endpoints

--- src/identity-service/src/server.js
+++ src/identity-service/src/server.js
@@ -22,6 +22,8 @@ mongoose
   .then(() => logger.info("Connected to mongodb"))
   .catch((e) => logger.error("Mongo connection error", e));
 
+const redisClient = new Redis(process.env.REDIS_URL);
+
 //middleware
 app.use(helmet());
 app.use(cors());
@@ -32,6 +34,42 @@ app.use((req, res, next) => {
   logger.info(`Request body, ${req.body}`);
   next();
 });
+
+//DDos protection and rate limiting
+const rateLimiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: "middleware",
+  points: 10,
+  duration: 1,
+});
+
+app.use((req, res, next) => {
+  rateLimiter
+    .consume(req.ip)
+    .then(() => next())
+    .catch(() => {
+      logger.warn(`Rate limit exceeded for IP: ${req.ip}`);
+      res.status(429).json({ success: false, message: "Too many requests" });
+    });
+});
+
+//Ip based rate limiting for sensitive endpoints
+const sensitiveEndpointsLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 50,
+  standardHeaders: true,
+  legacyHeaders: false,
+  handler: (req, res) => {
+    logger.warn(`Sensitive endpoint rate limit exceeded for IP: ${req.ip}`);
+    res.status(429).json({ success: false, message: "Too many requests" });
+  },
+  store: new RedisStore({
+    sendCommand: (...args) => redisClient.call(...args),
+  }),
+});
+
+//apply this sensitiveEndpointsLimiter to our routes
+app.use("/register", sensitiveEndpointsLimiter);
 
 app.use(passport.initialize());
 //Routes
