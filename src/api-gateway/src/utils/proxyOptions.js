dcd821f188a05961e810192c28916d1f4cf8048d
Adham Saeed <adhamsaeeed0500@gmail.com>
2025-08-09 16:27:20 +0300

    fix(api-gateway): add slash for proxyReqPathResolver

--- src/api-gateway/src/utils/proxyOptions.js
+++ src/api-gateway/src/utils/proxyOptions.js
@@ -1,4 +1,4 @@
-const proxyReqPathResolver = (req) => req.originalUrl.split('/').slice(2).join('/');
+const proxyReqPathResolver = (req) => '/' + req.originalUrl.split('/').slice(2).join('/');
 const proxyErrorHandler = (err, res, next) => {
   console.error("Proxy Error:", err.message);
   res.status(500).json({ error: "Proxy failed", details: err.message });
