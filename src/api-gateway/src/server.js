c440a91867011b82ea59b67a0f5c5f027730a3a2
Adham Saeed <adhamsaeeed0500@gmail.com>
2025-08-09 15:41:03 +0300

    chore(api-gateway): implement global rate limiting for protect services endpoints

--- src/api-gateway/src/server.js
+++ src/api-gateway/src/server.js
@@ -13,6 +13,23 @@ app.use(cors());
 app.use(express.json());
 app.use(ratelimitOptions);
 
+//rate limiting
+const ratelimitOptions = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+  handler: (req, res) => {
+    logger.warn(`Sensitive endpoint rate limit exceeded for IP: ${req.ip}`);
+    res.status(429).json({ success: false, message: "Too many requests" });
+  },
+  store: new RedisStore({
+    sendCommand: (...args) => redisClient.call(...args),
+  }),
+});
+
+app.use(ratelimitOptions);
+
 app.use((req, res, next) => {
   logger.info(`Received ${req.method} request to ${req.url}`);
   logger.info(`Request body, ${req.body}`);
